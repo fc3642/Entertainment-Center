9c8b9e43c8365949d5b9596e61c9d33bda936215
fc3642 <44911053+fc3642@users.noreply.github.com>
2018-11-23 10:43:12 -0800

    Update the file based on the reviewer's comments.

--- media.py
+++ media.py
@@ -1,22 +1,29 @@
 # import the required file for this program
 import webbrowser
 
+
 class Movie():
-    """This is the classes provides a way to store movie relateted information."""
+    """Is the classes provides a way to store movie relateted information."""
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
-    # define the class initiator, The movie title, story line, movie poster link, you tube tailer link and the year produced are the required parameters
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube, year):
+
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube, year):
+        """Defines the class initiator.
+
+        The movie title, story line, movie poster link, you tube tailer link
+        and the year produced are the required parameters.
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         self.year = year
 
-    # define the method to show movie trailer.
     def showTrailer(self):
+        """Defines the method to show movie trailer. """
         webbrowser.open(self.trailer_youtube_url)
 
-    # define the method to show movie poster image
     def showImage(self):
+        """Defines the method to show movie poster image. """
         webbrowser.open(self.poster_image_url)
